dbaf11d08e003759a8df402707a0b9bf0b0b740c
Jeremy gros <jeremy@aloa.us>
2018-04-22 13:32:38 +0200

    Fix start controller

---
+++ worldcup-pops/XYZ/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  worldcup-pops
+//
+//  Created by Jeremy gros on 22/04/2018.
+//  Copyright © 2018 Jeremy gros. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+UIApplicationMain(CommandLine.argc, UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc)), nil, NSStringFromClass(AppDelegate.self))
